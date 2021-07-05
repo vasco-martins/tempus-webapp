75edcac28b50bf1bad9f867586ad082f084123a4
Vasco Martins <vascomartins37@gmail.com>
2021-07-05 18:31:08 +0100

    Text

--- src/helpers/getUser.tsx
+++ src/helpers/getUser.tsx
@@ -11,27 +11,27 @@ export default async function getCurrentUser(ctx) {
     return {};
   }
 
-  try {
-    const user = await axios.get(
-      process.env.NEXT_PUBLIC_API_URL + "/auth/currentUser",
-      {
-        headers: {
-          Accept: "application/json",
-          Authorization: "Bearer " + cookies.token,
+  //  try {
+  const user = await axios.get(
+    process.env.NEXT_PUBLIC_API_URL + "/auth/currentUser",
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + cookies.token,
 
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-          "Content-Type": "application/json",
-        },
-      }
-    );
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-    return { token: cookies.token, user: user.data.data };
-  } catch (err) {
+  return { token: cookies.token, user: user.data.data };
+  /*} catch (err) {
     //   nookies.destroy(ctx, "token");
     ctx.res.statusCode = 302;
     ctx.res.setHeader("Location", `/auth/login`);
     ctx.res.end();
     return {};
-  }
+  }*/
 }
